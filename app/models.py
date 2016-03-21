120e564674f5dcd5ec0026904f2d2ab5b46b7b4f
Stephen Perl <shp425@cs.utexas.edu>
2016-03-21 17:47:22 -0500

    added models.py that reflects the 3 classes we are using for the project

--- app/models.py
+++ app/models.py
@@ -1,13 +1,37 @@
+# -------
+# Imports
+# -------
+
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
+# -----------
+# URL Routing
+# -----------
 
+class City(db.Model):
+    __tablename__ = 'cities'
 
+    city_id = db.Column(db.Integer, primary_key=True)
+    city_name = db.Column(db.String(256), nullable=False)
+    state_name = db.Column(db.String(256), nullable=False, foreign_key=True)
 
-# class Guest(db.Model):
-#     __tablename__ = 'guests'
+    def __repr__(self):
+        return "[City: city_id={}, city_name={}, state_name={}]".format(self.city_id, self.city_name, self.state_name)
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(256), nullable=False)
+class State(db.Model):
+	__tablename__ = 'states'
 
-#     def __repr__(self):
-#         return "[Guest: id={}, name={}]".format(self.id, self.name)
+	 state_name = db.Column(db.String(256), primary_key=True)
+
+	 def __repr__(self):
+        return "[State: state_name={}]".format(self.state_name)
+
+class Neighborhood(db.Model):
+	__tablename__ = 'neighborhoods'
+
+	neighborhood_id = db.Column(db.Integer, primary_key=True)
+	neighborhood_name = db.Column(db.Integer, primary_key=True)
+
+	def __repr__(self):
+		return "[Neighborhood: neighborhood_id={}, neighborhood_name={}".format(self.neighborhood_id, self.neighborhood_name)
