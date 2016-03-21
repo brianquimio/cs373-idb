f9032638c9cc049a4298a15af4fad3565075878d
Stephen Perl <shp425@cs.utexas.edu>
2016-03-20 21:33:20 -0500

    modified docker yml files and parred down the app.py file for just static HTML

--- app/app.py
+++ app/app.py
@@ -1,82 +1,109 @@
+
+
+# -------
+# Imports
+# -------
+
 import logging
 import os
+import Models
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(levelname)s: %(message)s')
-logger = logging.getLogger(__name__)
-logger.debug("Welcome to Carina Guestbook")
+# ------------------------------
+# Configure logger for debugging
+# ------------------------------
 
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     format='%(levelname)s: %(message)s')
+# logger = logging.getLogger(__name__)
+# logger.debug("Welcome to the Virtual Address Space")
+
+
+# -------------------------------------------
+# Configure MySQL Database and SQLAlchemy URI
+# -------------------------------------------
 
 SQLALCHEMY_DATABASE_URI = \
-    '{engine}://{username}:{password}@{hostname}/{database}'.format(
+    '{engine}://{username}:{password}@hostname}/{database}'.format(
         engine='mysql+pymysql',
         username=os.getenv('MYSQL_USER'),
         password=os.getenv('MYSQL_PASSWORD'),
         hostname=os.getenv('MYSQL_HOST'),
         database=os.getenv('MYSQL_DATABASE'))
 
-logger.debug("The log statement below is for educational purposes only. Do *not* log credentials.")
-logger.debug("%s", SQLALCHEMY_DATABASE_URI)
+
+# -------------
+# Configure app
+# -------------
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 manager = Manager(app)
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+# -----------
+# URL Routing
+# -----------
+
+@app.route('/')
 def index():
-    logger.debug("index")
+    # logger.debug("index")
+    return render_template('index.html')
 
-    if request.method == 'POST':
-        name = request.form['name']
-        guest = Guest(name=name)
-        db.session.add(guest)
-        db.session.commit()
-        return redirect(url_for('index'))
+@app.route('/showCities')
+def showCities():
+    # logger.debug("cities")
+    return render_template('cities.html')
 
-    return render_template('index.html', guests=Guest.query.all())
+@app.route('/showStates')
+def showCities():
+    # logger.debug("cities")
+    return render_template('states.html')
 
-
-class Guest(db.Model):
-    __tablename__ = 'guests'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
-
-    def __repr__(self):
-        return "[Guest: id={}, name={}]".format(self.id, self.name)
+@app.route('/showNeighborhoods')
+def showCities():
+    # logger.debug("cities")
+    return render_template('neighborhoods.html')
 
 
-@manager.command
-def create_db():
-    logger.debug("create_db")
-    app.config['SQLALCHEMY_ECHO'] = True
-    db.create_all()
+# ---------------------
+# Commands for database
+# ---------------------
 
-@manager.command
-def create_dummy_data():
-    logger.debug("create_test_data")
-    app.config['SQLALCHEMY_ECHO'] = True
-    guest = Guest(name='Steve')
-    db.session.add(guest)
-    db.session.commit()
+# @manager.command
+# def create_db():
+#     logger.debug("create_db")
+#     app.config['SQLALCHEMY_ECHO'] = True
+#     db.create_all()
 
-@manager.command
-def drop_db():
-    logger.debug("drop_db")
-    app.config['SQLALCHEMY_ECHO'] = True
-    db.drop_all()
+# @manager.command
+# def create_dummy_data():
+#     logger.debug("create_test_data")
+#     app.config['SQLALCHEMY_ECHO'] = True
+#     guest = Guest(name='Steve')
+#     db.session.add(guest)
+#     db.session.commit()
 
+# @manager.command
+# def drop_db():
+#     logger.debug("drop_db")
+#     app.config['SQLALCHEMY_ECHO'] = True
+#     db.drop_all()
+
+
+
+# -------
+# Run App
+# -------
 
 if __name__ == '__main__':
     manager.run()
---
+++ app/models.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+# class Guest(db.Model):
+#     __tablename__ = 'guests'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(256), nullable=False)
+
+#     def __repr__(self):
+#         return "[Guest: id={}, name={}]".format(self.id, self.name)
