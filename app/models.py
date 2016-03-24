dc96fbbb9c5724f4454f2310c35de2b06b881929
Stephen Perl <shperl20@gmail.com>
2016-03-23 19:01:50 -0500

    fixed models.py to match the UML diagram

--- app/models.py
+++ app/models.py
@@ -3,35 +3,67 @@
 # -------
 
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.app.builder
 
 
 # -----------
 # DB models
 # -----------
 
-class City(db.Model):
-    __tablename__ = 'cities'
+class State(db.Model):
+	__tablename__ = 'State'
 
+	# Dimensions
+	state_id = db.Column(db.Integer, primary_key=True)
+	date = db.Column(db.Date)
+	state_name = db.Column(db.String(256), unique=True)
+	property_type = db.Column(db.String(256))
+
+    # Measures
+    num_properties = db.Column(db.Integer)
+    avg_listing_price = db.Column(db.Integer)
+
+	
+
+	def __repr__(self):
+		return "[State: state_id={}, state_name={}]".format(self.state_id, self.state_name)
+
+class City(db.Model):
+    __tablename__ = 'City'
+
+    # Dimensions
     city_id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
     city_name = db.Column(db.String(256), nullable=False)
-    state_name = db.Column(db.String(256), nullable=False, foreign_key=True)
+    property_type = db.Column(db.String(256))
+
+    # Measures
+    num_properties = db.Column(db.Integer)
+    avg_listing_price = db.Column(db.Integer)
+
+    # Relationships
+    state_id = db.Column(db.Integer, db.ForeignKey('State.state_id'))
+
 
     def __repr__(self):
         return "[City: city_id={}, city_name={}, state_name={}]".format(self.city_id, self.city_name, self.state_name)
 
-class State(db.Model):
-	__tablename__ = 'states'
-
-	 state_name = db.Column(db.String(256), primary_key=True)
-
-	 def __repr__(self):
-        return "[State: state_name={}]".format(self.state_name)
-
 class Neighborhood(db.Model):
-	__tablename__ = 'neighborhoods'
+	__tablename__ = 'Neighborhood'
 
+	# Dimensions
 	neighborhood_id = db.Column(db.Integer, primary_key=True)
-	neighborhood_name = db.Column(db.Integer, primary_key=True)
+	date = db.Column(db.Date)
+	neighborhood_name = db.Column(db.Integer, nullable=False)
+
+	# Measures
+    num_properties = db.Column(db.Integer)
+    avg_listing_price = db.Column(db.Integer)
+
+    # Relationships
+    state_id = db.Column(db.Integer, db.ForeignKey('State.state_id'))
+    city_id = db.Column(db.Integer, db.ForeignKey('City.city_id'))
+
 
 	def __repr__(self):
 		return "[Neighborhood: neighborhood_id={}, neighborhood_name={}".format(self.neighborhood_id, self.neighborhood_name)
