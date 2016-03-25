47fe46375f5ba3e9b96424c7a5de15d4ca939060
Keith Gibson <kdgbsn@gmail.com>
2016-03-24 20:54:58 -0500

    added newest front-end: html, css, reference updates

--- app/templates/resources/js/app.js
+++ app/templates/resources/js/app.js
@@ -2,23 +2,21 @@
   var app = angular.module('app', [ ]);
   app.controller('containerGenerator', function(){
     this.containers = [];
-    this.containers.push(new container("'container-fluid regionTitleContainer'","SomeName", null, false));
-    this.containers.push(new container("'container-fluid dataContainer'","SomeName","SomeContent",true));
-    this.containers.push(new container("'container-fluid dataContainer'","SomeName2","SomeContent1231",true));
+    this.containers.push(new container("'container-fluid regionTitleContainer'","SomeName", null));
+    this.containers.push(new container("'container-fluid dataContainer'","SomeName","SomeContent"));
+    this.containers.push(new container("'container-fluid dataContainer'","SomeName2","SomeContent1231"));
 
   });
 
-  // var dataContainer = {
-  //   classes : "container-fluid dataContainer",
-  //   name : "SomeName",
-  //   content : "Some Data"
-  // };
+  function getPageData(uri){
 
-  function container(classes, name, content, hasContent){
+    return null;
+  };
+
+  function container(classes, name, content){
     this.classes = classes;
     this.name = name;
     this.content = content;
-    this.hasContent = hasContent;
   };
 
 })();
