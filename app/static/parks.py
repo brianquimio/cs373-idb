cb559984e683c8d2722c9a5d309dd3f3a6a4ae0b
Steven Zhu <stevenzhu94@utexas.edu>
2016-04-04 22:17:36 -0500

    changed apikey to api_key to match URL params, however xml styling still causing retrieval issues

--- app/static/parks.py
+++ app/static/parks.py
@@ -10,32 +10,29 @@ import requests, xmltodict, datetime
 class Parks(object):
 
 	"""
-    retrieves park data from the ACTIVE Campground API and returns it as an OrderedDict
-    """
+	retrieves park data from the ACTIVE Campground API and returns it as an OrderedDict
+	"""
 
-    def __init__ (self, apikey):
-    	self.apikey = apikey
-    	self.url = "http://api.amp.active.com/camping/campgrounds"
+	def __init__ (self, apikey):
+		self.apikey = apikey
+		self.url = "http://api.amp.active.com/camping/campgrounds"
 
+	#requested information to be returned in json, but online calls say "This XML file does not appear to have any style information associated with it."
+	#wiil need to test to see which information is being returned. 
 
-    #requested information to be returned in json, but online calls say "This XML file does not appear to have any style information associated with it."
-    #wiil need to test to see which information is being returned. 
+	def get_state_parks(self, pstate):
+		"""
+		state is two character code
+		returns an OrderedDict of stats about the park
+		"""
 
-    def get_state_parks(self, pstate):
-        """
-        state is two character  code
-        returns an OrderedDict of stats about the park
-        """
+		parameters = {
+			"pstate": pstate,
+			"api_key": self.apikey
+		}
 
+		xml = requests.get(self.url, params=parameters)
+		results = xmltodict.parse(xml.content)
+		retval = results["facilityName"]
 
-        parameters = {
-            "pstate": pstate,
-            "apikey": self.apikey
-        }
-
-        xml = requests.get(url, params=parameters)
-        results = xmltodict.parse(xml.content)
-        retval = results["facilityName"]
-
-
-        return retval
+		return retval
