88d6c30599e956f09a6639b40422f25975e35a74
Keith Gibson <kdgbsn@gmail.com>
2016-04-05 20:24:46 -0500

    added  to get static resources; progress on issue #17

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,22 +1,67 @@
 (function(){
-  var app = angular.module('app', [ ]);
-  app.controller('containerGenerator', function(){
-    this.containers = [];
-    this.containers.push(new container("'container-fluid regionTitleContainer'","SomeName", null));
-    this.containers.push(new container("'container-fluid dataContainer'","SomeName","SomeContent"));
-    this.containers.push(new container("'container-fluid dataContainer'","SomeName2","SomeContent1231"));
+  var app = angular.module('VASapp', ['ngResource']);
 
-  });
+  app.errorPage = "../error404.html";
+  app.nowhere= "#";
 
-  function getPageData(uri){
+  app.controller('mainController',
+    [ '$rootScope',
+      '$http',
+      function($rootScope, $http){
+        $rootScope.name = "Virtual Address Space";
+        $rootScope.currentPage = "Home";
+        $rootScope.data = {};
+        this.pageData = {};
+        this.followRef = function(link) {
+          // $rootScope.currentPage = link.display;
+          $http.get(link.ref)
+          .then(function(response) {
+            $rootScope.status = response.status;
+            $rootScope.data = response.data;
+            $rootScope.currentPage = $rootScope.data["name"];
+            }, function(response) {
+            $rootScope.data = response.data || "Request failed";
+            $rootScope.status = response.status;
+          })
+        };
+      }]);
 
-    return null;
-  };
+  app.controller('navbarController',
+    [ '$rootScope',
+      '$http',
+      function($rootScope, $http){
+      $rootScope.navLinks = [];
+      this.addLink = function(link) {
+        $rootScope.navLinks.push(link);
+      };
+      this.addLink(new Link("About", "/app/static/json/test-about.json"));
+      this.addLink(new Link("States", "/app/static/json/test-states.json"));
+      this.addLink(new Link("Cities", "/app/static/json/test-cities.json"));
+      this.addLink(new Link("Neighborhoods", "/app/static/json/test-neighborhoods.json"));
+      this.isActive = function(pageName){
+        return $rootScope.currentPage === pageName;
+      };
+  }]);
 
-  function container(classes, name, content){
-    this.classes = classes;
-    this.name = name;
-    this.content = content;
+  app.controller('contentController',
+    [ '$rootScope',
+      function($rootScope){
+        this.showContent = function(category){
+          // return false;
+          return $rootScope.data['category'] === category;
+        };
+  }]);
+
+    app.controller('tableController',
+      [ '$scope',
+        function($scope){
+          $scope.tableRows = $scope.data["table-rows"];
+
+  }]);
+
+  function Link(display, ref) {
+    this.display = display;
+    this.ref = ref;
   };
 
 })();
