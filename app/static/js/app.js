7fab6daf8e92f48628a8424a51ecfee78319ee5d
Keith Gibson <kdgbsn@gmail.com>
2016-04-06 20:51:47 -0500

    added table functionality and embedded maps

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,22 +1,101 @@
 (function(){
-  var app = angular.module('app', [ ]);
-  app.controller('containerGenerator', function(){
-    this.containers = [];
-    this.containers.push(new container("'container-fluid regionTitleContainer'","SomeName", null));
-    this.containers.push(new container("'container-fluid dataContainer'","SomeName","SomeContent"));
-    this.containers.push(new container("'container-fluid dataContainer'","SomeName2","SomeContent1231"));
+  var app = angular.module('VASapp', ['ngResource']);
 
-  });
+  app.errorPage = "../error404.html";
+  app.nowhere= "#";
 
-  function getPageData(uri){
+  app.controller('mainController',
+    [ '$rootScope',
+      '$http',
+      function($rootScope, $http){
+        $rootScope.name = "Virtual Address Space";
+        $rootScope.currentPage = "Home";
+        $rootScope.data = {};
+        this.pageData = {};
+        this.followRef = function(link) {
+          // $rootScope.currentPage = link.display;
+          $http.get(link.ref)
+          .then(function(response) {
+            $rootScope.status = response.status;
+            $rootScope.data = response.data;
+            $rootScope.currentPage = $rootScope.data["name"];
+            }, function(response) {
+            $rootScope.data = response.data || "Request failed";
+            $rootScope.status = response.status;
+          })
+        };
+      }]);
 
-    return null;
-  };
+  app.controller('navbarController',
+    [ '$rootScope',
+      '$http',
+      function($rootScope, $http){
+      $rootScope.navLinks = [];
+      this.addLink = function(link) {
+        $rootScope.navLinks.push(link);
+      };
+      this.addLink(new Link("About", "/app/static/json/test-about.json"));
+      this.addLink(new Link("States", "/app/static/json/test-states.json"));
+      this.addLink(new Link("Cities", "/app/static/json/test-cities.json"));
+      this.addLink(new Link("Neighborhoods", "/app/static/json/test-neighborhoods.json"));
+      this.isActive = function(pageName){
+        return $rootScope.currentPage === pageName;
+      };
+  }]);
 
-  function container(classes, name, content){
-    this.classes = classes;
-    this.name = name;
-    this.content = content;
+  app.controller('contentController',[ '$rootScope',function($rootScope){
+    this.showContent = function(category){
+      // return false;
+      return $rootScope.data['category'] === category;
+    };
+  }]);
+
+  app.controller('tableController',[ '$rootScope', function($rootScope){
+    this.showColumn = function (subCategory, depth) {
+      this.depth = this.showHelper(subCategory);
+      return this.depth >= depth;
+    };
+    this.showHelper = function(subCategory){
+      if (subCategory === "neighborhood") return 3;
+      if (subCategory === "city") return 2;
+      if (subCategory === "state") return 1;
+      return 0;
+    };
+  }]);
+
+  app.controller('mapController',['$rootScope', '$sce', function($rootScope, $sce){
+    this.mapSrcBuilder = function(data = $rootScope.data) {
+      var embedKey = "AIzaSyCADkkH1GoSKSlgVxk_oyLp6roM6XEx44I"
+      var q = "";
+      if (data.hasOwnProperty("neighborhood") && data["neighborhood"]){
+        q += data["neighborhood"].replace(/ /g, "+");
+        q += ',';
+      };
+      if (data.hasOwnProperty("city") && data["city"]){
+        q += data["city"].replace(/ /g, "+");
+        q += ',';
+      };
+      if (data.hasOwnProperty("state") && data["state"]){
+        q += data["state"].replace(/ /g, "+");
+        q += ',';
+      };
+      // var src = "https://maps.googleapis.com/maps/api/staticmap?center=";
+      // src += q;
+      // src += "&zoom=14&size=400x400&key=";
+      // src += embedKey;
+      var src = "https://www.google.com/maps/embed/v1/place?key="
+      src += embedKey;
+      src += "&q=";
+      src += q;
+      console.log(src);
+
+      return $sce.trustAsResourceUrl(src);
+    };
+  }]);
+
+  function Link(display, ref) {
+    this.display = display;
+    this.ref = ref;
   };
 
 })();
