cfc1a921431d676b6a44045c9d77d0aa0022cd80
Steven Zhu <stevenzhu94@utexas.edu>
2016-04-08 18:46:21 -0500

    modified tests

--- app/tests.py
+++ app/tests.py
@@ -9,20 +9,19 @@
 # Imports
 # -------
 
-from app            import db, app
+from app            import db, app, models
 from io             import StringIO
 from urllib.request import urlopen
 from unittest       import main, TestCase
-from models         import *
 
 
 TEST_DB_URI = "sqlite://"
 
-# --------------------
-# State and StateStats
-# --------------------
+class TestModels (TestCase):
 
-class TestState (TestCase):
+    #----------------------
+    # DB setup and teardown
+    #----------------------
 
     def make_app(self):
         app.config['TESTING'] = True
@@ -37,25 +36,52 @@ class TestState (TestCase):
         db.session.add(state1)
         db.session.add(state2)
         db.session.add(state3)
-        stat1 = StateStats('03-01-16', '1-bedroom', 9, 12345, 12000, 'TX')
-        stat2 = StateStats('03-01-16', '2-bedroom', 6, 11111, 12000, 'TX')
-        stat3 = StateStats('03-01-16', '3-bedroom', 3, 15678, 16000, 'TX')
-        db.session.add(stat1)
-        db.session.add(stat2)
-        db.session.add(stat3)
+        state_stat1 = StateStats('03-01-2016', '1-bedroom', 9, 12345, 12000, 'TX')
+        state_stat2 = StateStats('03-01-2016', '2-bedroom', 6, 11111, 12000, 'TX')
+        state_stat3 = StateStats('03-01-2016', '3-bedroom', 3, 15678, 16000, 'TX')
+        state_stat4 = StateStats('03-01-2016', '1-bedroom', 11, 17698, 18000, 'NY')
+        db.session.add(state_stat1)
+        db.session.add(state_stat2)
+        db.session.add(state_stat3)
+        db.session.add(state_stat4)
+        city1 = City(4, 'Austin', 'TX', '12.34', '5.123')
+        city2 = City(5, 'San Antonio', 'TX', '2.345', '4.000')
+        city3 = City(6, 'San Francisco', 'TX', '4.321', '16.000')
+        db.session.add(city1)
+        db.session.add(city2)
+        db.session.add(city3)
+        city_stat1 = CityStats('04-01-2016', 'TX', '1-bedroom', 10, 10000, 15000, 4)
+        city_stat2 = CityStats('04-01-2016', 'TX', '2-bedroom', 11, 11000, 16000, 4)
+        city_stat3 = CityStats('04-01-2016', 'TX', '3-bedroom', 9, 12000, 17000, 4)
+        city_stat4 = CityStats('04-01-2016', 'TX', '1-bedroom', 5, 14900, 15000, 5)
+        db.session.add(city_stat1)
+        db.session.add(city_stat2)
+        db.session.add(city_stat3)
+        db.session.add(city_stat4)
+        neighborhood1 = Neighborhood(6, 'West University', 'TX', 4)
+        neighborhood2 = Neighborhood(7, 'Twin Peaks', 'TX', 4)
+        db.session.add(neighborhood1)
+        db.session.add(neighborhood2)
+        neighborhood_stat1 = NeighborhoodStats('01-31-16', 6, '1-bedroom', 50, 11111, 10000)
+        db.session.add(neighborhood_stat1)
         db.session.commit()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
 
+
+    # --------------------
+    # State and StateStats
+    # --------------------
+
     def test_state_1(self):
         states = State.query.all()
-        self.assertTrue(len(states) == 3)
+        self.assertTrue(len(states) == 4)
 
     def test_state_2(self):
         states = State.query.filter_by(state_id > 1)
-        self.assertTrue(len(states) == 2)
+        self.assertTrue(len(states) == 3)
 
     def test_state_3(self):
         state = State.query.filter_by(state_id = 2)
@@ -71,7 +97,7 @@ class TestState (TestCase):
 
     def test_state_stat_1(self):
         stats = StateStats.query.filter_by(property_type = '1-bedroom')
-        self.assertEqual(9, stats.num_properties)
+        self.assertEqual(11, stats[1].num_properties)
 
     def test_state_stat_2(self):
         stats = StateStats.query.filter_by(state_code = 'TX')
@@ -82,57 +108,29 @@ class TestState (TestCase):
         self.assertEqual(3, stats[2].num_properties)
 
 
-
-# ------------------
-# City and CityStats
-# ------------------
-
-class TestCity (TestCase):
-
-    def make_app(self):
-        app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = TEST_DB_URI
-        return app
-
-    def setUp(self):
-        db.create_all()
-        city1 = City(4, 'Austin', 'TX', '12.34', '5.123')
-        city2 = City(5, 'San Antonio', 'TX', '2.345', '4.000')
-        city3 = City(6, 'San Francisco', 'TX', '4.321', '16.000')
-        db.session.add(city1)
-        db.session.add(city2)
-        db.session.add(city3)
-        city_stat1 = CityStats('04-01-2016', 'TX', '1-bedroom', 10, 10000, 15000, 4)
-        city_stat2 = CityStats('04-01-2016', 'TX', '2-bedroom', 11, 11000, 16000, 4)
-        city_stat2 = CityStats('04-01-2016', 'TX', '3-bedroom', 9, 12000, 17000, 4)
-        db.session.add(city_stat1)
-        db.session.add(city_stat2)
-        db.session.add(city_stat3)
-        db.session.commit()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+    # ------------------
+    # City and CityStats
+    # ------------------
 
     def test_city_1(self):
         cities = City.query.all()
-        self.assertTrue(len(cities) == 3)
+        self.assertTrue(len(cities) == 4)
 
     def test_city_2(self):
         cities = City.query.filter_by(state_code == 'TX')
-        self.assertTrue(len(cities) == 2)
+        self.assertTrue(len(cities) == 3)
 
     def test_city_3(self):
         cities = City.query.filter_by(state_code == 'TX')
         self.assertEqual('Austin', cities[0].city_name)
 
     def test_city_4(self):
-        city = City.query.filter_by(city_code == 'NY')
+        city = City.query.filter_by(city_code == 'CA')
         self.assertTrue(city is None)
 
     def test_citystats_1(self):
         stats = CityStats.query.filter_by(state_code = 'TX')
-        self.assertTrue(len(stats) == 3)
+        self.assertTrue(len(stats) == 4)
 
     def test_citystats_2(self):
         stats = CityStats.query.filter_by(state_code = 'TX')
@@ -143,32 +141,9 @@ class TestCity (TestCase):
         self.assertEqual(12000, stats[2].avg_listing_price)
 
 
-
-
-# -----------------------------------
-# Neightborhood and NeighborhoodStats
-# -----------------------------------
-
-class TestNeighborhood (TestCase):
-
-    def make_app(self):
-        app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = TEST_DB_URI
-        return app
-
-    def setUp(self):
-        db.create_all()
-        neighborhood1 = Neighborhood(6, 'West University', 'TX', 4)
-        neighborhood2 = Neighborhood(7, 'Twin Peaks', 'TX', 4)
-        db.session.add(neighborhood1)
-        db.session.add(neighborhood2)
-        neighborhood_stat1 = NeighborhoodStats('01-31-16', 6, '1-bedroom', 50, 11111, 10000)
-        db.session.add(neighborhood_stat1)
-        db.session.commit()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+    # -----------------------------------
+    # Neightborhood and NeighborhoodStats
+    # -----------------------------------
 
     def test_neighborhood_1(self):
         nb = Neighborhood.query.all()
